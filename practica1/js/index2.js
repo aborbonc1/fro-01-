d5859589919d43b7379528b7ffade5c14c300191
aborbonc1 <abornonc@ucenfotec.ac.cr>
2022-08-20 09:58:51 -0600

    Creación carpeta practica 1
    Se agregan los archivos de la practica 1

---
+++ practica1/js/index2.js
@@ -0,0 +1,15 @@
+//let num1=7;
+//let num2=9;
+//let resultado1=num1+num2;
+//let resultado2=num1*num2;
+//console.log('La suma es de:', resultado1);
+//console.log('La multiplicación es de:', resultado2);
+
+let h2Nombre= document.getElementById('nombre-personaje');
+
+h2Nombre.textContent=h2Nombre.textContent.toUpperCase();
+//imprimir el texto  que tiene el elemento adentro
+
+console.log(h2Nombre.textContent);
+
+
